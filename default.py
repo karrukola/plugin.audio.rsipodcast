65e37e90c7c926457b5cea9eec8a9be14c2836f1
Filippo Carra <filippo.carra@gmail.com>
2016-10-22 12:13:37 +0200

    First working bit

--- default.py
+++ default.py
@@ -10,6 +10,11 @@ from json import loads
 
 BASEURL = 'http://il.srgssr.ch/integrationlayer/1.0/ue/rsi/'
 
+CHLIST = [
+    ('rete-tre',
+     'http://www.rsi.ch/play/assets/img/srg/rsi/rete_tre_wide.png')
+]
+
 
 def build_url(query):
     """
@@ -19,12 +24,13 @@ def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
 
-def get_shows(channelId):
+def get_shows(channelid):
     """
     Return a json object with all the shows for a certain channel
     """
     url = BASEURL + 'radio/assetGroup/editorialPlayerAlphabeticalByChannel/' + \
-        channelId + '.json'
+            channelid + '.json'
+    print url
     resp = requests.get(url=url)
     data = loads(resp.text)
     return data['AssetGroups']['Show']
@@ -45,47 +51,87 @@ def get_episode_audio_url(episodeId):
     return data['Audio']['Playlists']['Playlist'][0]['url'][0]['text']
 
 
-def play_episode(episodeId):
+def play_episode(episodeId, addon_handle):
     epUrl = get_episode_audio_url(episodeId)
+    li = xbmcgui.ListItem(path=epUrl)
+    xbmcplugin.setResolvedUrl(addon_handle, True, li)
 
     return epUrl
 
 
-def add_show(show):
-    print "#####"
-    print show['id']
-    print show['title'].encode('utf-8').strip()
+def add_show(show, addon_handle):
+    """
+    Add a list item for each show
+    """
     try:
-        print show['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
+        thumb = show['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
     except:
-        print "default image"
-    print ""
+        thumb = None
 
+    li = xbmcgui.ListItem(label=show['title'].encode('utf-8').strip(),
+                          thumbnailImage=thumb)
+    xbmcplugin.addDirectoryItem(handle=addon_handle,
+                                url=build_url({'mode': 'AssetGroup',
+                                               'rsiid': show['id']}),
+                                listitem=li,
+                                isFolder=True)
     return
 
 
-def add_episode(episode):
+def add_episode(ep, addon_handle):
+    """
+    Add a list item for each episode
+    """
+    try:
+        thumb = ep['Assets']['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
+    except:
+        thumb = None
+    li = xbmcgui.ListItem(label=ep['title'].encode('utf-8').strip(),
+                          thumbnailImage=thumb)
+    li.setProperty('IsPlayable', 'true')
+    xbmcplugin.addDirectoryItem(handle=addon_handle,
+                                url=build_url({'mode': 'play',
+                                               'rsiid': ep['id']}),
+                                listitem=li)
     return
 
 
 def main():
+    """
+    main function, handles modes and lists creation in Kodi
+    """
     args = urlparse.parse_qs(sys.argv[2][1:])
     mode = args.get('mode', None)
+    rsiid = args.get('rsiid', None)
     if mode is None:
-        # # !!!ToDo!!! when mode is None create list of channels. You must find a way to include channels' icons
-        # showsList = get_shows('rete-tre')
-        # for show in showsList:
-        #     add_show(show)
+        # create list of (radio) channels
+        for channel in CHLIST:
+            li = xbmcgui.ListItem(label=channel[0],
+                                  thumbnailImage=channel[1])
+            xbmcplugin.addDirectoryItem(handle=addon_handle,
+                                        url=build_url({'mode': 'channel', 'rsiid': channel[0]}),
+                                        listitem=li,
+                                        isFolder=True)
+    elif mode[0] == 'channel':
+        showsList = get_shows(rsiid[0])
+        for show in showsList:
+            add_show(show, addon_handle)
+    elif mode[0] == 'AssetGroup':
+        episodesList = get_episodes(rsiid[0])
+        for episode in episodesList:
+            add_episode(episode, addon_handle)
+    elif mode[0] == 'play':
+        play_episode(rsiid[0], addon_handle)
 
-        # episodesList = get_episodes('3703891')
-        # for episode in episodesList:
-        #     print episode['title'].encode('utf-8').strip()
+    # e chiudiamo la lista per tutti i modi
+    xbmcplugin.endOfDirectory(addon_handle)
 
-        epUrl = play_episode('8092102')
-        play_item = xbmcgui.ListItem(path=epUrl)
-        xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
+    #     print episode['title'].encode('utf-8').strip()
+
+    # epUrl = play_episode('8092102')
+    # play_item = xbmcgui.ListItem(path=epUrl)
+    # xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
 
 if __name__ == '__main__':
-    # sample_page = BASEURL + 'radio/assetGroup/editorialPlayerAlphabeticalByChannel/rete-tre.json'
     addon_handle = int(sys.argv[1])
     main()
