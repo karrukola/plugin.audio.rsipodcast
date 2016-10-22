76c11637000ff34a7690256c43243093e5ad9826
Filippo Carra <filippo.carra@gmail.com>
2016-10-22 08:27:51 +0200

    second commit, now using the same JSON data structures of the app

--- default.py
+++ default.py
@@ -1,11 +1,3 @@
-"""
-Main module of Kodi (XBMC) addon to manage RSI.ch podcast section.
-
-This is the main module that takes care of receiving the information from the
-parser and present it in Kodi creating the appropriate pages and feeding the
-URL of the media content to the player.
-"""
-
 import os
 import sys
 import urllib
@@ -13,33 +5,87 @@ import urlparse
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
+import requests
+from json import loads
 
-args = urlparse.parse_qs(sys.argv[2][1:])
-mode = args.get('mode', None)
-addon_handle = int(sys.argv[1])
+BASEURL = 'http://il.srgssr.ch/integrationlayer/1.0/ue/rsi/'
+
 
 def build_url(query):
+    """
+    Kodi passess paramteres through a URL that is used to call the plugin itself
+    """
     base_url = sys.argv[0]
     return base_url + '?' + urllib.urlencode(query)
 
 
-url_hdcd = 'http://mediaww.rsi.ch/rsi/unrestricted/2016/06/13/2139211.mp3?content-disposition-attachment'
+def get_shows(channelId):
+    """
+    Return a json object with all the shows for a certain channel
+    """
+    url = BASEURL + 'radio/assetGroup/editorialPlayerAlphabeticalByChannel/' + \
+        channelId + '.json'
+    resp = requests.get(url=url)
+    data = loads(resp.text)
+    return data['AssetGroups']['Show']
+
+
+def get_episodes(showId):
+    url = BASEURL + 'assetSet/listByAssetGroup/' + showId + '.json'
+    resp = requests.get(url=url)
+    data = loads(resp.text)
+    return data['AssetSets']['AssetSet']
+
+
+def get_episode_audio_url(episodeId):
+    url = BASEURL + 'audio/play/' + episodeId + '.json'
+    print url
+    resp = requests.get(url=url)
+    data = loads(resp.text)
+    return data['Audio']['Playlists']['Playlist'][0]['url'][0]['text']
+
+
+def play_episode(episodeId):
+    epUrl = get_episode_audio_url(episodeId)
+
+    return epUrl
+
+
+def add_show(show):
+    print "#####"
+    print show['id']
+    print show['title'].encode('utf-8').strip()
+    try:
+        print show['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
+    except:
+        print "default image"
+    print ""
+
+    return
+
+
+def add_episode(episode):
+    return
+
+
+def main():
+    args = urlparse.parse_qs(sys.argv[2][1:])
+    mode = args.get('mode', None)
+    if mode is None:
+        # # !!!ToDo!!! when mode is None create list of channels. You must find a way to include channels' icons
+        # showsList = get_shows('rete-tre')
+        # for show in showsList:
+        #     add_show(show)
+
+        # episodesList = get_episodes('3703891')
+        # for episode in episodesList:
+        #     print episode['title'].encode('utf-8').strip()
+
+        epUrl = play_episode('8092102')
+        play_item = xbmcgui.ListItem(path=epUrl)
+        xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
 
 if __name__ == '__main__':
-    if mode is None:
-        li = xbmcgui.ListItem(label='pippo')
-        li.setProperty('IsPlayable', 'true')
-        url = build_url({'mode': 'stream', 'url': url_hdcd, 'title': 'pippo'})
-        xbmcplugin.addDirectoryItem(handle=addon_handle,
-                                        url=url,
-                                        listitem=li) 	
-        xbmcplugin.endOfDirectory(addon_handle)
-
-
-    elif mode[0] == 'stream':
-        urllalla = args['url'][0]
-        play_item = xbmcgui.ListItem(path=urllalla)
-        print 'url'
-        print urllalla
-        xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
-        xbmcplugin.endOfDirectory(addon_handle)
+    # sample_page = BASEURL + 'radio/assetGroup/editorialPlayerAlphabeticalByChannel/rete-tre.json'
+    addon_handle = int(sys.argv[1])
+    main()
