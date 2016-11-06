4b705e0b78744f1e850d6fd1f95c6f01363781c1
Filippo Carra <filippo.carra@gmail.com>
2016-11-06 18:37:37 +0100

    Handling of background and show images

--- default.py
+++ default.py
@@ -28,6 +28,19 @@ def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
 
+def gen_image_url(imgid, usage):
+    """Generata image url based on its id and usage
+    Possible values for usage:
+    WEBVISUAL (980x550)
+    EPISODE_IMAGE (624x351)
+    """
+    if imgid is not None:
+        xbmc.log('imgid is not None')
+        return 'http://www.rsi.ch/rsi-api/resize/image/' + usage + '/' + imgid + '/'
+    else:
+        xbmc.log('imgid is None')
+        return None
+
 def get_shows(channelid):
     """
     Return a json object with all the shows for a certain channel
@@ -83,31 +96,29 @@ def add_show(show):
     Add a list item for each show
     """
     try:
-        thumb = show['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
+        imgid = show['Image']['ImageRepresentations']['ImageRepresentation'][0]['id']
+        thumb = gen_image_url(imgid, 'EPISODE_IMAGE')
     except:
+        imgid = None
         thumb = None
 
     kli = xbmcgui.ListItem(label=show['title'].encode('utf-8').strip(),
                            thumbnailImage=thumb)
     xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
                                 url=build_url({'mode': 'AssetGroup',
-                                               'rsiid': show['id']}),
+                                               'rsiid': show['id'],
+                                               'imgid': imgid}),
                                 listitem=kli,
                                 isFolder=True)
     return
 
 
-def add_episode(ep):
+def add_episode(ep, fanart_url):
+    """Add a list item for each episode
     """
-    Add a list item for each episode
-    """
-    try:
-        thumb = ep['Assets']['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
-    except:
-        thumb = None
-    kli = xbmcgui.ListItem(label=ep['title'].encode('utf-8').strip(),
-                           thumbnailImage=thumb)
+    kli = xbmcgui.ListItem(label=ep['title'].encode('utf-8').strip())
     kli.setProperty('IsPlayable', 'true')
+    kli.setArt({'fanart' : fanart_url})
     xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
                                 url=build_url({'mode': 'play',
                                                'rsiid': ep['id']}),
@@ -115,7 +126,7 @@ def add_episode(ep):
     return
 
 
-def add_next_page(page, rsiid):
+def add_next_page(page, rsiid, imgid):
     """
     Add navigation to another page
     """
@@ -127,7 +138,8 @@ def add_next_page(page, rsiid):
     xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE,
                                 url=build_url({'mode': 'AssetGroup',
                                                'rsiid': rsiid[0],
-                                               'page': str(nextpage)}),
+                                               'page': str(nextpage),
+                                               'imgid': imgid}),
                                 listitem=kli,
                                 isFolder=True)
     return
@@ -141,6 +153,7 @@ def main():
     mode = args.get('mode', None)
     rsiid = args.get('rsiid', None)
     page = args.get('page', None)
+    imgid = args.get('imgid', None)
     if mode is None:
         # create list of (radio) channels
         for channel in CHLIST:
@@ -155,11 +168,15 @@ def main():
         for show in showslist:
             add_show(show)
     elif mode[0] == 'AssetGroup':
+        if imgid is not None:
+            imgid = imgid[0]
+        # thumb_url = gen_image_url(imgid[0], 'EPISODE_IMAGE')
+        fanart_url = gen_image_url(imgid, 'WEBVISUAL')
         episodeslist, pagesize = get_episodes(rsiid[0], page)
         for episode in episodeslist:
-            add_episode(episode)
+            add_episode(episode, fanart_url)
         if pagesize != 0:
-            add_next_page(page, rsiid)
+            add_next_page(page, rsiid, imgid)
     elif mode[0] == 'play':
         play_episode(rsiid[0])
 
